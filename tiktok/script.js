2d050b6b92bf2dd087a19d37556bc1c852aa8512
soutozk <154080694+soutozk@users.noreply.github.com>
2024-01-29 20:38:38 -0300

    Add files via upload

---
+++ tiktok/script.js
@@ -0,0 +1,11 @@
+function showTextBox() {
+    var heartBtn = document.getElementById('heart-btn');
+    var textBox = document.getElementById('text-box');
+  
+    // Oculta o coração
+    heartBtn.style.display = 'none';
+  
+    // Exibe o retângulo com texto
+    textBox.style.display = 'block';
+  }
+  
